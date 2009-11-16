ab4ba22b9735ad7457ff74a80557629f62123b93
ianlesnet <ianlesnet@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2009-11-16 12:29:18 +0000

    Christmas card and ornament 2009 files.

---
+++ trunk/holiday-cards/09-Christmas/firmware/main.c
@@ -0,0 +1,61 @@
+/*
+ * This file is part of a project posted at Dangerous Prototypes (http://dangerousprototypes.com)
+ * Project: Christmas card ornament '09
+ *
+ * To the extent possible under law, Dangerous Prototypes (Ian Lesnet) has
+ * waived all copyright and related or neighboring rights to this code. This
+ * work is published from United States.
+ *
+ * For details see: http://creativecommons.org/publicdomain/zero/1.0/.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include <avr/io.h>
+#include <avr/sleep.h>
+#define NOP __asm__ __volatile__ ("nop")
+
+void delay_cnt(unsigned char c);
+
+int main(void){
+	unsigned char i, j;
+
+	//configure chip
+	//select clock source
+	DDRB&=(~0xff); 				//pins outputs
+	PORTB|=(0xff);				//pins GND
+	
+	for(i=0; i<10; i++){ 		//flash the lights 10 times
+
+		//we don't just blink them, we fade them in and out
+		for(j=0; j<0xff; j++){	//fade in gently
+			PORTB|=(0xff);		//LEDs on
+			delay_cnt(j);		//delay (grows)
+			PORTB&=(~0xff);		//LEDs off
+			delay_cnt(0xff-j);	//delay (shrinks)
+		}
+		
+		for(j=0; j<0xff; j++){	//fade out gently
+			PORTB&=(~0xff);		//LEDs off
+			delay_cnt(j);		//delay (grows)
+			PORTB|=(0xff);		//LEDs on
+			delay_cnt(0xff-j);	//delay (shrinks)
+		}
+
+	}
+
+	//sleep forever, reset/replace battery to run again
+	PORTB&=(~0xff);				//LEDs off
+	MCUCR = _BV(SM1) |  _BV(SE);//configure sleep mode   
+  	sleep_cpu();   				//requires sleep.h
+
+}
+
+//simple time wasting delay function
+void delay_cnt(unsigned char c){ 
+	unsigned char i;
+
+	for(i=0; i<c; i++) NOP; //waste time
+}
