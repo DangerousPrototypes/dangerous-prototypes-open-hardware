dc61b9a11479b40a50b309038a2ca2da59c8dc35
m.gritsch <m.gritsch@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-03-06 09:21:45 +0000

    Use ICS_PGD1 instead of the incorrect 0b11.

--- web-platform/firmware/ds33Intro/main.c
+++ web-platform/firmware/ds33Intro/main.c
@@ -10,7 +10,7 @@
 _FOSCSEL(FNOSC_FRCPLL)		//INT OSC with PLL (always keep this setting)
 _FOSC(OSCIOFNC_OFF & POSCMD_NONE)	//disable external OSC (always keep this setting)
 _FWDT(FWDTEN_OFF)				//watchdog timer off
-_FICD(JTAGEN_OFF & 0b11);//JTAG debugging off, debugging on PG1 pins enabled
+_FICD(JTAGEN_OFF & ICS_PGD1);//JTAG debugging off, debugging on PG1 pins enabled
 
 unsigned char UART1RXRdy(void);
 unsigned char UART1RX(void);
