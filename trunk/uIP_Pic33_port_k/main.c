26a98e11b5131fb553c5cdc0c5940ac7fa02a95c
engkan2kit <engkan2kit@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-03-09 16:48:55 +0000

    Checked and modified files from the AVR port and added them to the project file pic33_uIP.mcp. This current version compiles.

--- trunk/uIP_Pic33_port_k/main.c
+++ trunk/uIP_Pic33_port_k/main.c
@@ -4,7 +4,10 @@
 /* \C30\support\dsPIC33F\gld\ */
 
 #include "HardwareProfile.h"
+#include "uip.h"
+#include "uip_arp.h"
 #include "enc28j60.h"
+#include "nic.h"
 
 //it's important to keep configuration bits that are compatibale with the bootloader
 //if you change it from the internall/PLL clock, the bootloader won't run correctly
@@ -12,7 +15,10 @@ _FOSCSEL(FNOSC_FRCPLL)		//INT OSC with PLL (always keep this setting)
 _FOSC(OSCIOFNC_OFF & POSCMD_NONE)	//disable external OSC (always keep this setting)
 _FWDT(FWDTEN_OFF)				//watchdog timer off
 _FICD(JTAGEN_OFF & 0b11);//JTAG debugging off, debugging on PG1 pins enabled
-
+void initTimer(void)
+{
+//timer init routine here.
+}
 
 int main(void)
 { //main function, execution starts here
@@ -24,10 +30,28 @@ int main(void)
 	CLKDIVbits.PLLPRE=0; // PLLPRE (N2) 0=/2 
 	PLLFBD=41; //pll multiplier (M) = +2
 	CLKDIVbits.PLLPOST=0;// PLLPOST (N1) 0=/2
+    
+   
     while(!OSCCONbits.LOCK);//wait for PLL ready
-    enc28j60Init(); // at this point uIP is not used yet
+    nic_init(); // Initialize ENC28j60. At this point uIP is not used yet
+
+    // init uIP
+    uip_init();
+
+    // init app
+    example1_init();
+
+    // init ARP cache
+    uip_arp_init();
+
+    // init periodic timer
+    initTimer();
+
+     
+    
     while(1)
     {//never ending loop
+    
 
 	}
 }
