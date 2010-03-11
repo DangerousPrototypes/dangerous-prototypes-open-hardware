8b956ac080fcfffb04992b5a5f46150960e0e64e
engkan2kit <engkan2kit@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-03-11 14:17:02 +0000

    I added a hardware reset on the initialization of ENC on enc28j60.c. I put a small delay between the toggling from low to high. Then the following lines from enc28j60:
    #define CS_EN()    ENC_CS_LAT=0 
    #define CS_DIS()   ENC_CS_LAT=1
    #define HARDRESET() ENC_RST_LAT = 0; delay_ms(10);ENC_RST_LAT = 1
    #define SPI_BUF     ENC_SSPBUF
    #define SPITXRX() 	while(!ENC_SPI_IF)
    
    were move to HardwareProfile.h because they are hardware dependent functions and its possible that we might call them on the main function.

--- trunk/uIP_Pic33_port_k/delay.c
+++ trunk/uIP_Pic33_port_k/delay.c
@@ -3,22 +3,7 @@
 
 void delay_ms(unsigned char ms)
 {
-   unsigned short delay_count = F_CPU / 4000;
-/*#ifdef 0
-    unsigned short cnt;
-    asm volatile ("\n"
-                  "L_dl1%=:\n\t"
-                  "mov %A0, %A2\n\t"
-                  "mov %B0, %B2\n"
-                  "L_dl2%=:\n\t"
-                  "sbiw %A0, 1\n\t"
-                  "brne L_dl2%=\n\t"
-                  "dec %1\n\t" "brne L_dl1%=\n\t":"=&w" (cnt)
-                  :"r"(ms), "r"((unsigned short) (delay_count))
-        );
-#else
-*/
-    unsigned short delay_cnt = F_CPU/6000;
+    unsigned short delay_cnt = F_CPU/1000;
     //unsigned short delay_cnt = 2400;   //*KU* for 14.745600 MHz Clock
     unsigned short delay_cnt_buffer;
 
@@ -26,7 +11,7 @@ void delay_ms(unsigned char ms)
         delay_cnt_buffer = delay_cnt;
         while (delay_cnt_buffer--);
     }
-//#endif
+
 }
 
 
