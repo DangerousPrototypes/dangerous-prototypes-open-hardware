93d4d0ab64e967062a2e774bad482e096785f945
ianlesnet <ianlesnet@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-06-03 09:33:11 +0000

    Flash Destroyer. New compiled firmware and batch files.

---
+++ FlashDestroyer/update-FD-selftext-v1.bat
@@ -0,0 +1,2 @@
+fw_update -e -w -v -m flash -vid 0x04D8 -pid 0xFBEB -ix FD-firmware-selftest-v1.hex
+pause
--- FlashDestroyer/update-FD-v1.bat
+++
@@ -1,2 +0,0 @@
-fw_update -e -w -v -m all -vid 0x04D8 -pid 0xFBEB -ix FD-firmware-v1.hex
-pause
