d4b0080b029069c539291ee1c1c628aeb0fa1383
impaktakiller@yahoo.com.ph <impaktakiller@yahoo.com.ph@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-07-07 13:58:51 +0000

    still in progress

--- trunk/USBIRtoy/software/wxpythonApp/IrDraw.py
+++ trunk/USBIRtoy/software/wxpythonApp/IrDraw.py
@@ -8,17 +8,37 @@ class IrDraw:
             
     def OnPaint(self, event):
         dc = wx.PaintDC(self.__myFrame)
-        dc.SetPen(wx.Pen('red', 2))
-        x=20
+        x=100
         y=100
         x_offset=30
-        y_offset=30
+        y_offset=20
+        y_AddToHorizontalLine=40
+        
+        dc.SetFont(wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, \
+            wx.FONTWEIGHT_BOLD, False, 'Courier 10 Pitch'))
+        
+        
+        ##Draw Upper and Lower Line
+        x_guideLine_Offset=960
+        y_guideLine_Offset=3
+        #lower
+        dc.SetPen(wx.Pen('black', 1))
+        dc.DrawLine(x, y+y_guideLine_Offset, x+x_guideLine_Offset, y+y_guideLine_Offset)
+        dc.SetPen(wx.Pen('blue', 1,wx.DOT))
+        #upper
+        dc.DrawLine(x, y-y_guideLine_Offset-y_offset, x+x_guideLine_Offset, y-y_guideLine_Offset-y_offset)
         
         lst=0xAAAAAAAA # TODO: put this outside
         IsLogicLow=True
         lstPoints=[wx.Point(x,y)]
         ctr=0
+        dc.SetPen(wx.Pen('black', 1,wx.DOT))
         while(ctr<32):
+            #draw horizontal line
+            dc.DrawLine(x,y,x,y-y_AddToHorizontalLine)
+            
+            dc.DrawText(str(ctr),x+2,y-40)
+            
             if((lst&(1<<ctr))!=0):
                 if(IsLogicLow):
                     lstPoints.append(wx.Point(x,y-y_offset))
@@ -31,6 +51,7 @@ class IrDraw:
                 lstPoints.append(wx.Point(x+x_offset,y))
             ctr+=1    
             x+=x_offset
+        dc.SetPen(wx.Pen('red', 2))
         dc.DrawLines(lstPoints) 
             
     def DrawHi(self,x,y):
--- trunk/USBIRtoy/software/wxpythonApp/main.py
+++ trunk/USBIRtoy/software/wxpythonApp/main.py
@@ -3,13 +3,14 @@
 import wx
 import IrDraw
 
-STR_TITLE='IR Decoder 00.08.00'
+STR_TITLE='IR Decoder 00.08.01 Powered by wxPython'
 
 def create(parent):
     return Frame1(parent)
 
-[wxID_FRAME1, wxID_FRAME1PANEL1, 
-] = [wx.NewId() for _init_ctrls in range(2)]
+[wxID_FRAME1, wxID_FRAME1BUTTON1, wxID_FRAME1CHOICECOMPORT, wxID_FRAME1PANEL1, 
+ wxID_FRAME1PANEL2, 
+] = [wx.NewId() for _init_ctrls in range(5)]
 
 class Frame1(wx.Frame):
     
@@ -18,21 +19,54 @@ class Frame1(wx.Frame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_FRAME1, name='', parent=prnt,
-              pos=wx.Point(122, 204), size=wx.Size(1034, 195),
+              pos=wx.Point(97, 455), size=wx.Size(1140, 164),
               style=wx.DEFAULT_FRAME_STYLE, title='Frame1')
-        self.SetClientSize(wx.Size(1026, 161))
+        self.SetClientSize(wx.Size(1132, 130))
+        self.SetHelpText('')
+        self.SetToolTipString('Frame1')
+        self.SetWindowVariant(wx.WINDOW_VARIANT_SMALL)
+        self.SetIcon(wx.Icon(u'c:/Dev-Cpp/Icons/Software.ico',
+              wx.BITMAP_TYPE_ICO))
 
         self.panel1 = wx.Panel(id=wxID_FRAME1PANEL1, name='panel1', parent=self,
-              pos=wx.Point(0, 0), size=wx.Size(1026, 161),
+              pos=wx.Point(0, 0), size=wx.Size(1132, 130),
               style=wx.TAB_TRAVERSAL)
+
+        self.panel2 = wx.Panel(id=wxID_FRAME1PANEL2, name='panel2',
+              parent=self.panel1, pos=wx.Point(24, 8), size=wx.Size(288, 40),
+              style=wx.TAB_TRAVERSAL)
+        self.panel2.SetBackgroundStyle(wx.BG_STYLE_SYSTEM)
+        self.panel2.SetBackgroundColour(wx.Colour(188, 205, 190))
+
+        self.button1 = wx.Button(id=wxID_FRAME1BUTTON1, label='Connect',
+              name='button1', parent=self.panel2, pos=wx.Point(16, 8),
+              size=wx.Size(120, 24), style=0)
+        self.button1.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL, False,
+              'Tahoma'))
+
+        self.choiceComPort = wx.Choice(choices=[], id=wxID_FRAME1CHOICECOMPORT,
+              name='choiceComPort', parent=self.panel2, pos=wx.Point(152, 8),
+              size=wx.Size(125, 24), style=0)
+        self.choiceComPort.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL,
+              False, 'Tahoma'))
 
     def __init__(self, parent):
         self._init_ctrls(parent)
         self.SetLabel(STR_TITLE)
-        #self.MyIrDraw=IrDraw.IrDraw(self)
         self.MyIrDraw=IrDraw.IrDraw(self.panel1)
+        self.WindowStyle
         self.Centre()
+        self.SetMaxSize(self.GetSize())
+        self.SetMinSize(self.GetSize())
         self.Show(True)
+        self.choiceComPort.Clear()
+        ComList=[]
+        for x in range(1,100):
+            ComList.append('COM' + str(x))
+        self.choiceComPort.SetItems(ComList)
+        #self.choiceComPort.SetItems(['hehe','heherhe'])
+        self.choiceComPort.Select(0)
+
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
