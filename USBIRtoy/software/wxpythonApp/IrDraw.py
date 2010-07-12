ab89ade4c7c987144244ccb6ebad6ce96d2dec1b
impaktakiller <impaktakiller@yahoo.com.ph>
2010-07-12 14:29:22 +0000

    new rev

--- USBIRtoy/software/wxpythonApp/IrDraw.py
+++ USBIRtoy/software/wxpythonApp/IrDraw.py
@@ -1,45 +1,85 @@
 import wx
 
 class IrDraw:
+    
+    __NUMBITSOVERVIEW=512
+    __NUMBITSDETAILED=32
+    
     def __init__(self,MyFrame):
+        LIST_SIZE=40
         self.__myFrame=MyFrame
         self.__myFrame.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.__DataToDraw=[0]*(8*LIST_SIZE)
         
+    def GetDataBytesList(self):
+        #return [0xAA]*(self.__NUMBITSOVERVIEW/8)
+        return range(0,5000)
             
     def OnPaint(self, event):
         dc = wx.PaintDC(self.__myFrame)
-        x=100
-        y=100
-        x_offset=30
-        y_offset=20
-        y_AddToHorizontalLine=40
+        broad_x=5
+        broad_y=50
+        broad_x_offset=2
+        broad_y_offset=20
+        
+        detailed_x=5
+        detailed_y=100
+        detailed_x_offset=32
+        detailed_y_offset=20
+        
+        NUM_COLUMN_DETAILED_WAVEFORM=10
+        #y_AddToHorizontalLine=40
         
         dc.SetFont(wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, \
             wx.FONTWEIGHT_BOLD, False, 'Courier 10 Pitch'))
         
-        
-        ##Draw Upper and Lower Line
-        x_guideLine_Offset=960
-        y_guideLine_Offset=3
-        #lower
-        dc.SetPen(wx.Pen('black', 1))
-        dc.DrawLine(x, y+y_guideLine_Offset, x+x_guideLine_Offset, y+y_guideLine_Offset)
-        dc.SetPen(wx.Pen('blue', 1,wx.DOT))
-        #upper
-        dc.DrawLine(x, y-y_guideLine_Offset-y_offset, x+x_guideLine_Offset, y-y_guideLine_Offset-y_offset)
-        
-        lst=0xAAAAAAAA # TODO: put this outside
-        IsLogicLow=True
-        lstPoints=[wx.Point(x,y)]
+        #Broad WaveForm
+        self.GenWaveListWaveForm(dc, wx.Pen('blue', 1),\
+            self.GetNBitListPoints(self.__NUMBITSOVERVIEW,self.GetDataBytesList(),broad_x,broad_y,broad_x_offset,broad_y_offset))
+            
+            
+        #Detailed WaveForm
+        self.DrawMultipleDetailedWaveForms(dc,wx.Pen('black',1),  \
+            self.GetDataBytesList(),NUM_COLUMN_DETAILED_WAVEFORM, \
+            detailed_x,detailed_y,detailed_x_offset,detailed_y_offset)
+            
+        #draw Vertical Lines
+        y_AddToVertLine=450
+        dc.SetPen(wx.Pen('GREY',1,wx.DOT))
         ctr=0
-        dc.SetPen(wx.Pen('black', 1,wx.DOT))
-        while(ctr<32):
-            #draw horizontal line
-            dc.DrawLine(x,y,x,y-y_AddToHorizontalLine)
-            
-            dc.DrawText(str(ctr),x+2,y-40)
-            
-            if((lst&(1<<ctr))!=0):
+        while(ctr<(self.__NUMBITSDETAILED+1)):
+            x=detailed_x+(ctr*detailed_x_offset)
+            dc.DrawLine(x,detailed_y,x,detailed_y+y_AddToVertLine)
+            ctr+=1
+        
+
+    def DrawMultipleDetailedWaveForms(self,dc,pen,lstData,numColumn,detailed_x,detailed_y,detailed_x_offset,detailed_y_offset):
+        ctr=0
+        DETAILED_Y_INCREMENT=50
+        while(ctr<numColumn):
+            index=ctr*(self.__NUMBITSDETAILED/8)
+            self.GenWaveListWaveForm(dc, pen, \
+                        self.GetNBitListPoints(self.__NUMBITSDETAILED,lstData[index:(index+4)]  , \
+                        detailed_x,detailed_y,detailed_x_offset,detailed_y_offset)) 
+            ctr+=1 
+            detailed_y+=DETAILED_Y_INCREMENT
+
+   
+    
+    def GenWaveListWaveForm(self,dc,pen,lstDataBytes):
+        if(pen!=None):
+            dc.SetPen(pen)
+        dc.DrawLines(lstDataBytes) 
+        
+
+        
+    def GetNBitListPoints(self,NumBits,DataList,x,y,x_offset,y_offset):
+        lstPoints=[wx.Point(x,y)]
+        IsLogicLow=True
+        ctr=0
+       
+        while(ctr<(NumBits)):
+            if((DataList[ctr/8]&(1<<(ctr%8)))!=0):
                 if(IsLogicLow):
                     lstPoints.append(wx.Point(x,y-y_offset))
                     IsLogicLow=False
@@ -50,15 +90,36 @@ class IrDraw:
                     IsLogicLow=True
                 lstPoints.append(wx.Point(x+x_offset,y))
             ctr+=1    
-            x+=x_offset
-        dc.SetPen(wx.Pen('red', 2))
-        dc.DrawLines(lstPoints) 
-            
-    def DrawHi(self,x,y):
-        pass
+            x+=x_offset  
+        return lstPoints    
     
-    def DrawLo(self,x,y):
-        pass
 
 
-##########
+########## EVERYTHING BELOW WILL BE USED AS SCRATCH
+
+
+
+   
+##    def DrawNBitPoints(self,NumByte,Data,x,y,x_offset,y_offset):
+##        lstPoints=[]
+##        IsLogicLow=True
+##        ctr=0
+##        while(ctr<(NumByte*8)):
+##            #draw horizontal line
+##            #dc.DrawLine(x,y,x,y-y_AddToHorizontalLine)
+##            
+##            #dc.DrawText(str(ctr),x+2,y-40)
+##            
+##            if((Data&(1<<ctr))!=0):
+##                if(IsLogicLow):
+##                    lstPoints.append(wx.Point(x,y-y_offset))
+##                    IsLogicLow=False
+##                lstPoints.append(wx.Point(x+x_offset,y-y_offset))
+##            else:
+##                if(IsLogicLow==False):
+##                    lstPoints.append(wx.Point(x,y))
+##                    IsLogicLow=True
+##                lstPoints.append(wx.Point(x+x_offset,y))
+##            ctr+=1    
+##            x+=x_offset  
+##        return lstPoints 
--- USBIRtoy/software/wxpythonApp/main.py
+++ USBIRtoy/software/wxpythonApp/main.py
@@ -1,6 +1,7 @@
 #Boa:Frame:Frame1
 
 import wx
+import wx.lib.analogclock
 import IrDraw
 
 STR_TITLE='IR Decoder 00.08.01 Powered by wxPython'
@@ -8,9 +9,10 @@ STR_TITLE='IR Decoder 00.08.01 Powered by wxPython'
 def create(parent):
     return Frame1(parent)
 
-[wxID_FRAME1, wxID_FRAME1BUTTON1, wxID_FRAME1CHOICECOMPORT, wxID_FRAME1PANEL1, 
- wxID_FRAME1PANEL2, 
-] = [wx.NewId() for _init_ctrls in range(5)]
+[wxID_FRAME1, wxID_FRAME1ANALOGCLOCK1, wxID_FRAME1BUTTON1, 
+ wxID_FRAME1CHOICECOMPORT, wxID_FRAME1PANEL1, wxID_FRAME1PNLWAVEFORM, 
+ wxID_FRAME1STATICBOX1, 
+] = [wx.NewId() for _init_ctrls in range(7)]
 
 class Frame1(wx.Frame):
     
@@ -19,9 +21,9 @@ class Frame1(wx.Frame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_FRAME1, name='', parent=prnt,
-              pos=wx.Point(97, 455), size=wx.Size(1140, 164),
+              pos=wx.Point(485, 187), size=wx.Size(1080, 698),
               style=wx.DEFAULT_FRAME_STYLE, title='Frame1')
-        self.SetClientSize(wx.Size(1132, 130))
+        self.SetClientSize(wx.Size(1072, 664))
         self.SetHelpText('')
         self.SetToolTipString('Frame1')
         self.SetWindowVariant(wx.WINDOW_VARIANT_SMALL)
@@ -29,31 +31,43 @@ class Frame1(wx.Frame):
               wx.BITMAP_TYPE_ICO))
 
         self.panel1 = wx.Panel(id=wxID_FRAME1PANEL1, name='panel1', parent=self,
-              pos=wx.Point(0, 0), size=wx.Size(1132, 130),
+              pos=wx.Point(0, 0), size=wx.Size(1072, 664),
               style=wx.TAB_TRAVERSAL)
 
-        self.panel2 = wx.Panel(id=wxID_FRAME1PANEL2, name='panel2',
-              parent=self.panel1, pos=wx.Point(24, 8), size=wx.Size(288, 40),
-              style=wx.TAB_TRAVERSAL)
-        self.panel2.SetBackgroundStyle(wx.BG_STYLE_SYSTEM)
-        self.panel2.SetBackgroundColour(wx.Colour(188, 205, 190))
+        self.staticBox1 = wx.StaticBox(id=wxID_FRAME1STATICBOX1,
+              label='COM Port Settings', name='staticBox1', parent=self.panel1,
+              pos=wx.Point(16, 8), size=wx.Size(280, 56), style=0)
+        self.staticBox1.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL,
+              False, 'Tahoma'))
 
         self.button1 = wx.Button(id=wxID_FRAME1BUTTON1, label='Connect',
-              name='button1', parent=self.panel2, pos=wx.Point(16, 8),
+              name='button1', parent=self.panel1, pos=wx.Point(32, 32),
               size=wx.Size(120, 24), style=0)
         self.button1.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL, False,
               'Tahoma'))
 
         self.choiceComPort = wx.Choice(choices=[], id=wxID_FRAME1CHOICECOMPORT,
-              name='choiceComPort', parent=self.panel2, pos=wx.Point(152, 8),
+              name='choiceComPort', parent=self.panel1, pos=wx.Point(160, 32),
               size=wx.Size(125, 24), style=0)
         self.choiceComPort.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL,
               False, 'Tahoma'))
 
+        self.analogClock1 = wx.lib.analogclock.analogclock.AnalogClock(id=wxID_FRAME1ANALOGCLOCK1,
+              name='analogClock1', parent=self.panel1, pos=wx.Point(312, 16),
+              size=wx.Size(80, 48), style=0)
+        self.analogClock1.SetFaceBorderWidth(2)
+        self.analogClock1.SetBackgroundColour(wx.Colour(192, 192, 192))
+
+        self.pnlWaveForm = wx.Panel(id=wxID_FRAME1PNLWAVEFORM,
+              name='pnlWaveForm', parent=self.panel1, pos=wx.Point(16, 72),
+              size=wx.Size(1040, 576), style=wx.TAB_TRAVERSAL)
+        self.pnlWaveForm.SetBackgroundColour(wx.Colour(192, 192, 192))
+
     def __init__(self, parent):
         self._init_ctrls(parent)
         self.SetLabel(STR_TITLE)
-        self.MyIrDraw=IrDraw.IrDraw(self.panel1)
+        self.MyIrDraw=IrDraw.IrDraw(self.pnlWaveForm)
+        #self.MyIrDraw=IrDraw.IrDraw(self.scrollWindowsWaveForm)
         self.WindowStyle
         self.Centre()
         self.SetMaxSize(self.GetSize())
@@ -66,6 +80,14 @@ class Frame1(wx.Frame):
         self.choiceComPort.SetItems(ComList)
         #self.choiceComPort.SetItems(['hehe','heherhe'])
         self.choiceComPort.Select(0)
+        #self.scrollWindowsWaveForm.SetVirtualSize(wx.Size(1000, 1000))
+        #self.scrollWindowsWaveForm.SetScrollbars(20,20,1000,1000)
+
+        
+
+    def OnSliderWaveFormViewCommandScroll(self, event):
+        self.choiceComPort.Select(self.sliderWaveFormView.GetValue())
+        event.Skip()
 
 
 if __name__ == '__main__':
