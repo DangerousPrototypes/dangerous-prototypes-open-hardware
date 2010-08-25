179d4996d29a6c59246a1ced3d0fe3d91a31a763
impaktakiller@yahoo.com.ph <impaktakiller@yahoo.com.ph@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-08-25 11:19:12 +0000

    Added to consolidate all the headers in a single place.

---
+++ trunk/USBIRtoy/IRtoy-firmware/globals.h
@@ -0,0 +1,40 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+// i put all the
+
+// =================== GLOBAL TYPEDEFS
+typedef unsigned char u8;
+typedef unsigned int u16;
+
+
+// =================== GLOBAL HEADERS
+#include "HardwareProfile.h"
+#include "GenericTypeDefs.h"
+#include "Compiler.h"
+#include "usb_config.h"
+
+#if 0
+#include "USBStack\usb.h"
+#include "USBStack\usb_device.h"
+#include "USBStack\usb_function_cdc.h"
+#endif
+
+#if 1
+//USB stack
+#include "./USB/usb.h"
+#include "./USB/usb_function_cdc.h"
+#include ".\USB\usb_device.h"
+#endif
+
+//IR Toy functions
+#include "SUMP.h" 		//sump functions
+#include "IRIO.h" 		//IRIO functions
+#include "IRs.h" 		//IRs functions
+#include "RCdecoder.h" 	//RC5 decoder
+
+//our USB input buffer functions
+#include "USBbuf.h"
+
+
+#endif
