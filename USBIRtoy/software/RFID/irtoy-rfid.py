1d4a83d2e68619dde1a570b0b07c6c7ad05ceee0
ianlesnet <ianlesnet@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-10-07 07:01:49 +0000

    added license header to IR Toy RFID script.

--- USBIRtoy/software/RFID/irtoy-rfid.py
+++ USBIRtoy/software/RFID/irtoy-rfid.py
@@ -1,3 +1,8 @@
+#http://dangerousprototypes.com
+#script to read RFID from USB IR Toy USB->serial converter
+#resets IR Toy, enters UART mode
+# this demo code is released into the public domain (Creative Commons 0)
+#
 import serial,time
 IsUsbIrToyUsed=True
 COMPort='COM17' # Change this if needed :)
