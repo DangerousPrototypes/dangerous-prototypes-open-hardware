9b306dbf46128f15e4be9ceb33eb2d52500b8af4
jamz <jamz@wherelabs.com>
2010-11-18 02:24:22 +0000

    Updated IRTOY to record and play  text files using the -t command .  The generated   file can then be edited using text editors. Separated each c functions to different c files.

---
+++ USBIRtoy/software/IRToyRecPlayV0.5/bin.h
@@ -0,0 +1,26 @@
+#ifndef BIN_H_INCLUDED
+#define BIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <time.h>
+#ifdef _WIN32
+#include <conio.h>
+#include <windef.h>
+#include <windows.h>
+#else
+#include <sys/select.h>
+#include <ncurses.h>
+#include <stdbool.h>
+#include <unistd.h>
+#endif
+
+#include "serial.h"
+
+int IRrecord(	char *,int );
+void IRplay(	char *,int,char *);
+
+#endif // BIN_H_INCLUDED
---
+++ USBIRtoy/software/IRToyRecPlayV0.5/ols.h.save
@@ -0,0 +1,27 @@
+#ifndef OLS_H_INCLUDED
+#define OLS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <time.h>
+#ifdef _WIN32
+#include <conio.h>
+#include <windef.h>
+#include <windows.h>
+#else
+#include <sys/select.h>
+#include <ncurses.h>
+#include <stdbool.h>
+#include <unistd.h>
+#endif
+
+
+void create_ols(char *);
+
+#endif // OLS_H_INCLUDED
+
+
+
