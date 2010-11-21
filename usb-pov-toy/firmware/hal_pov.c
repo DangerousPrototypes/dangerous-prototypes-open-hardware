63d308510560195772f0eac26bc5c3f694a5fe70
impaktakiller <impaktakiller@yahoo.com.ph>
2010-11-21 09:37:54 +0000

    Newly Added files

---
+++ usb-pov-toy/firmware/hal_pov.c
@@ -0,0 +1,34 @@
+#include "globals.h"
+
+
+
+u8 BitMapRam[BITMAP_RAM_SZ];
+
+void hal_pov_adjustDelay()
+{
+//TODO
+}
+
+
+void hal_pov_setupIntFromAclPin(void)
+{
+// TODO this is for the interrupt of accelerometer
+}
+
+
+void hal_pov_loadBitmapFromEeToRam()
+{
+// TODO
+}
+
+
+void hal_pov_displayBitmap()
+{
+// TODO this will use
+}
+
+
+void hal_pov_checkButtonAndChangeBitmap(void)
+{
+// TODO
+}
---
+++ usb-pov-toy/firmware/hal_pov.h
@@ -0,0 +1,6 @@
+#ifndef HAL_POV_H
+#define HAL_POV_H
+
+#define BITMAP_RAM_SZ	200 //TODO: adjust
+
+#endif
