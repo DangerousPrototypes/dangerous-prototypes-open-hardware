3d6edf848c8f5667afb55125b86bb477a41be093
ewalker@ieee.org <ewalker@ieee.org@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-11-22 00:47:23 +0000

    Create tag 0.0.0 of Web Platform FirmwareA2.

---
+++ trunk/web-platform/firmwareA2/tags/0.0.0/src/nic/nic_dma.c
@@ -0,0 +1,102 @@
+#include "nic_dma.h"
+#include "nic.h"
+#include "HardwareProfile.h"
+#include <stdio.h>
+#include "../rtos/task.h"
+volatile union SHORT_BITFIELD UIP_DMA_STATUS ;
+volatile unsigned int uip_dma_rx_pending_packets;
+
+unsigned int uip_dma_rx_last_packet_length;
+unsigned int uip_dma_tx_last_packet_length;
+
+
+
+
+
+
+//process INT from ENC. Start RX DMA if buffer free, otherwise
+//note theres one more packet waiting to be read.
+void __attribute__ ((interrupt,auto_psv)) _CNInterrupt(){
+	
+	if( ENC_INT_IO == 0 ) //INT pin gone low...it's real
+	{
+		
+		if( UIP_DMA_BUFFER_FREE )
+		{
+			UIP_DMA_BUFFER_FREE=0;			
+			if( 0 == ( uip_dma_rx_pending_packets = NICBeginPacketRetrieveDMA()) )
+			{
+				//got the interrupt, but no packet to read??? Reset buffer status
+				UIP_DMA_BUFFER_STATUS_RESET();
+			}
+		}
+		else
+		{
+			//dma not free, will have to collect later
+			uip_dma_rx_pending_packets++;
+		}
+	}	
+	IFS1bits.CNIF = 0; //clear CN int flag
+}
+
+/*
+* We'll have this interrupt handler for the dummy DMA channel
+* It will trigger the end of packet transmission because
+* we cannot lower CS until the END of the transmission 
+* (ie, AFTER the dummy has READ the last byte )
+*/
+void ENC_DMADUMMY_INTERRUPT(){
+	enc28j60EndPacketSend();
+	if( 0 == ( uip_dma_rx_pending_packets = NICBeginPacketRetrieveDMA() ) )
+	{
+		UIP_DMA_BUFFER_STATUS_RESET();
+	}
+	ENC_DMADUMMY_INTIF = 0;	
+}
+extern xSemaphoreHandle dmaSemaphore;
+
+void ENC_DMA_INTERRUPT(){
+	signed portBASE_TYPE pxHigherPriorityTaskWoken = pdFALSE;
+	if( ENC_DMACONbits.DIR == 0 ) //DIR 0 = read from peripheral to RAM
+	{
+		//do rx finished cleanup
+		enc28j60EndPacketReceive();
+		xSemaphoreGiveFromISR( dmaSemaphore, &pxHigherPriorityTaskWoken);
+	}
+	else
+	{
+		//end of packet send handled by other DMA channel
+	}
+	
+	ENC_DMA_INTIF = 0; //clear DMA int flag
+	if ( pxHigherPriorityTaskWoken != pdTRUE) {
+		taskYIELD();
+	}
+}
+
+//I want to write to the buffer, if this returns non-zero, I can
+unsigned int uip_acquireBuffer(){
+	unsigned int retval = 0;
+	if( UIP_DMA_BUFFER_FREE == 0 ) return 0;//quick test
+	IEC1bits.CNIE = 0; //disable CN int
+	if( UIP_DMA_BUFFER_FREE == 1 )
+	{
+		UIP_DMA_BUFFER_FREE=0;
+		retval = 1;
+	}
+	IEC1bits.CNIE = 1; //enable CN int
+
+	return retval;
+}
+
+void uip_releaseBuffer()
+{
+	if( ENC_DMACONbits.CHEN == 1 ) return;
+	IEC1bits.CNIE = 0; //disable CN int
+	if( ENC_DMACONbits.CHEN == 0  )
+	{
+		UIP_DMA_BUFFER_FREE=1;
+	}
+	IEC1bits.CNIE = 1; //enable CN int
+	
+}
---
+++ trunk/web-platform/firmwareA2/tags/0.0.0/src/proc.h
@@ -0,0 +1,16 @@
+/*
+** generic header file for dsPIC33F
+*/
+#if defined(__dsPIC33FJ128GP804__)
+
+#include "p33FJ128GP804.h"
+
+#elif defined(__dsPIC33FJ128GP204__)
+
+#include "p33FJ128GP204.h"
+
+#else
+
+#error -- processor ID not specified in project header file
+
+#endif
