ebd67f39550f3f913138012ee22d168db711c3c7
ewalker@ieee.org <ewalker@ieee.org@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-11-22 00:29:19 +0000

    Initial commit of Web Platform FirmwareA2.

---
+++ trunk/web-platform/firmwareA2/trunk/src/uip/uip_apps.h
@@ -0,0 +1,20 @@
+/*
+ * Contains app state which is common to all uIP apps
+ */
+#ifndef _UIP_APPS_H
+#define _UIP_APPS_H
+
+
+#include "../httpd/webserver.h"
+#include "../dhcpc/dhcpc.h"
+
+typedef union {
+	struct httpd_state httpd;
+} uip_tcp_appstate_t;
+
+typedef union {
+	// struct dhcpc_state dhcpc;
+} uip_udp_appstate_t;
+
+
+#endif
