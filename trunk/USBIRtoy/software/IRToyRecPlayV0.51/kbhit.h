40496168c9ab1a05c3616a853b8add688f681f99
jamz@wherelabs.com <jamz@wherelabs.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-11-22 23:11:56 +0000

    Remove kbhit version for linux, and provide a simple way so that capturing still works. May have produce other problem in linux.. windows still works fine

---
+++ trunk/USBIRtoy/software/IRToyRecPlayV0.51/kbhit.h
@@ -0,0 +1,14 @@
+#ifndef KBHIT_H_INCLUDED
+#define KBHIT_H_INCLUDED
+
+#ifndef KBHITh
+#define KBHITh
+
+#include <curses.h>
+#include <stdio.h>
+
+int      kbhit(void);
+
+#endif
+
+#endif // KBHIT_H_INCLUDED
