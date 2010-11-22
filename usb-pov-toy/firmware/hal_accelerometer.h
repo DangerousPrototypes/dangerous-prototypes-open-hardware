a53121e0adfb7095b61d029fc5ffa72c86a31a48
ianlesnet <ianlesnet@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-11-22 17:52:19 +0000

    USB POV toy tests and interrupt setup.

--- usb-pov-toy/firmware/hal_accelerometer.h
+++ usb-pov-toy/firmware/hal_accelerometer.h
@@ -2,7 +2,7 @@
 #define HAL_ACCELEROMETER_H
 
 #define ACL_CS LATAbits.LATA2
-#define ACL_INT1 PORTAbits.RB1
+#define ACL_INT1 PORTAbits.RA1
 #define ACL_INT_SETUP() TRISAbits.TRISA1=1
 #define LVLTH 0x1A
 #define INTRST 0x17
@@ -12,12 +12,18 @@
 typedef enum
 {
 ACL_REVERSE=0,
-FORWARD
+ACL_FORWARD
 }HAL_ACL_DIRECTION;
 
+	//xxZYXxxx
+#define ACL_X 0b1000
+#define ACL_Y 0b10000
+#define ACL_Z 0b100000
+#define CTL1_CFG ACL_Y | ACL_Z //disable unwanted interrupts
+#define ACL_DIRECTION 0x06
 
-
-
+HAL_ACL_DIRECTION hal_acl_IsItReverseOrForward(void);
+void hal_acl_config(void);
 u8 hal_acl_read(u8 r);
 void hal_acl_enable(void);
 void hal_acl_write(u8 r, u8 v);
