ac9c12109debfa8de1ad0205d096c284c55f9b6d
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2010-12-01 16:54:34 +0000

    FT232 universal USB LCD backpack software and hardware commit.

---
+++ trunk/LCD-backpacks/USB-universal-LCD/Software/McZ-app/source/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace USB_LCD_Backpack
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
