f4cb553fb15d241664641593c32c69e611d0ff23
ianlesnet <ianlesnet@gmail.com>
2010-12-13 13:32:51 +0000

    logic shrimp firmware

--- LogicShrimp/firmware/src/hal_logicshrimp.c
+++ LogicShrimp/firmware/src/hal_logicshrimp.c
@@ -5,5 +5,7 @@
 
 void hal_logicshrimp_init(void)
 {
-hal_logicshrimp_setClockGate(ON);
+hal_logicshrimp_pinsetup();
+hal_logicshrimp_setClockGate(OFF);
+hal_logicshrimp_setBufferEnable(OFF);
 }
--- LogicShrimp/firmware/src/hal_logicshrimp.h
+++ LogicShrimp/firmware/src/hal_logicshrimp.h
@@ -2,7 +2,7 @@
 #define HAL_LOGICSHRIMP_H
 
 //#define CLK_GATE	LATCbits.LATC0
-
+#define hal_logicshrimp_pinsetup() TRISCbits.TRISC0=0; TRISBbits.TRISB5=0
 #define hal_logicshrimp_setClockGate(x)				LATCbits.LATC0=x
 #define hal_logicshrimp_setBufferEnable(x)			LATBbits.LATB5=x
 
