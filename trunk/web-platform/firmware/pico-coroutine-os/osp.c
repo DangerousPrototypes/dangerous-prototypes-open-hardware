a64b976ed2d019406ee17d66502c16ef8836169f
voidptr69@gmail.com <voidptr69@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-02-10 04:33:20 +0000

    added basic semaphore and P V services

--- trunk/web-platform/firmware/pico-coroutine-os/osp.c
+++ trunk/web-platform/firmware/pico-coroutine-os/osp.c
@@ -3,20 +3,10 @@
 #include <stdlib.h>
 
 #include <setjmp.h>
-/*
-#ifndef	_SETJMP_H_
-#define	_SETJMP_H_
 
-#ifdef __C30__
-typedef unsigned int jmp_buf[18];
-#else
-typedef unsigned int jmp_buf[4];
-#endif
-extern	int	setjmp(jmp_buf);
-extern void	longjmp(jmp_buf, int);
-#endif	
+#include "osp.h"
 
-*/
+
 
 
 #define TASKS_SIZE 4
@@ -24,20 +14,67 @@ extern void	longjmp(jmp_buf, int);
 
 
 typedef enum { 
-	os_init = 0, os_exit, task_preempt, task_exit 
-}os_enum;
+    Clear = 0x0000, Running = 0x0001, Ready = 0x0002, Block = 0x0004, 
+}os_tstate;
 
 typedef struct {
-    int id;
+    int state;
+    int sem;
     jmp_buf regs;
-    unsigned stack[TASK_STACK_SIZE];
+    unsigned short stack[TASK_STACK_SIZE];
 } taskType;
-
 
 taskType tasks[TASKS_SIZE];
 int tasksSize;
-int taskCurr;
+int taskIndex;
+
+
+typedef enum { 
+	os_init = 0, os_exit, task_schedule, task_exit 
+}os_enum;
+
 jmp_buf osRegs;
+
+
+
+#define SEM_SIZE 3
+
+typedef struct {
+    int value;
+} semType;
+semType sems[SEM_SIZE];
+
+void osSemInit(int s, int v)
+{
+   sems[s].value = v; 
+}
+
+void osSemP(int s)
+{    
+    --sems[s].value;    
+    if (sems[s].value < 0) {  
+        tasks[taskIndex].sem = s; 
+        tasks[taskIndex].state = Block;
+        if (!setjmp( tasks[taskIndex].regs)) {
+            longjmp(osRegs, task_schedule);
+        }
+    }
+}
+
+void osSemV(int s)
+{
+    int i;
+    
+    ++sems[s].value; 
+    if (sems[s].value <= 0) {
+        i = taskIndex; 
+        do {
+            i = (i + 1) % TASKS_SIZE;
+        } while ( tasks[i].sem != s); 
+        tasks[i].sem = -1;
+        tasks[i].state = Ready;
+    }
+}
 
 
 void osInit(void)
@@ -45,26 +82,38 @@ void osInit(void)
     int i;
 
     tasksSize = 0; 
-    taskCurr = -1;
-    for(i = 0; i < TASKS_SIZE; i++) tasks[i].id = 0;
+    for(i = 0; i < TASKS_SIZE; i++) {
+        tasks[i].state = Clear;
+        tasks[i].sem = -1; 
+    }
+    
+    for(i = 0; i < SEM_SIZE; i++) { 
+        sems[i].value = 1;
+    }
 }
 
 void osBegin(void)
 {
     switch (setjmp(osRegs)) {
-        case task_preempt:
-            for ( ; !tasks[ (taskCurr = ++taskCurr % TASKS_SIZE) ].id; );
-            longjmp(tasks[taskCurr].regs, task_preempt);
+        case task_schedule:         
+            do {
+                taskIndex = (taskIndex + 1) % TASKS_SIZE;
+            } while ( tasks[taskIndex].state != Ready); 
+            
+            tasks[taskIndex].state = Running;
+            longjmp(tasks[taskIndex].regs, task_schedule);
             break;
         case task_exit:
-            tasks[taskCurr].id = 0;
+            tasks[taskIndex].state = Clear;
+            tasks[taskIndex].sem = -1;
             if (--tasksSize) {
-                longjmp(osRegs, task_preempt);
+                longjmp(osRegs, task_schedule);
             }
             break;
         case os_init:
             if (tasksSize) {
-                longjmp(osRegs, task_preempt);
+                taskIndex = -1;
+                longjmp(osRegs, task_schedule);
             }
             break;
         case os_exit:
@@ -79,27 +128,21 @@ void osExit(void)
 
 int osTaskCreate(void (*taskPtr)(void))
 {
-    int i;
+    int i = 0;
 
-    if (tasksSize < TASKS_SIZE) {
-        
-        for ( i = 0; tasks[i].id > 0; i++ );
-        tasks[i].id = i+1;
+    if (tasksSize < TASKS_SIZE) {       
+        for ( i = 0; tasks[i].state != Clear; i++ );
         ++tasksSize;
-        setjmp( tasks[i].regs );
-       
-        //stack frame pointer
-        tasks[i].regs[14] = (unsigned int)&tasks[i].stack[0];
-        //stack pointer
-        tasks[i].regs[15] = (unsigned int)&tasks[i].stack[0]; 
-        //pc low
-        tasks[i].regs[16] = (unsigned int)taskPtr;
-        //pc high
-        tasks[i].regs[17] = 0;
+        tasks[taskIndex].sem = -1;
+        tasks[i].state = Ready;
         
-        return(i+1);
+        setjmp( tasks[i].regs );
+        tasks[i].regs[14] = (unsigned int)&tasks[i].stack[0];
+        tasks[i].regs[15] = (unsigned int)&tasks[i].stack[0];  
+ 		tasks[i].regs[16] = (unsigned int)taskPtr;
+		tasks[i].regs[17] = 0;        
     }
-    return( 0 );
+    return( i );
 }
 
 void osTaskExit(void)
@@ -109,10 +152,10 @@ void osTaskExit(void)
 
 void osTaskPreempt(void)
 {
-    if (!setjmp(tasks[taskCurr].regs)) {
-        longjmp(osRegs, task_preempt);
+    if (!setjmp( tasks[taskIndex].regs)) {
+        tasks[taskIndex].state = Ready;
+        longjmp(osRegs, task_schedule);
     }
 }
-
 
 
