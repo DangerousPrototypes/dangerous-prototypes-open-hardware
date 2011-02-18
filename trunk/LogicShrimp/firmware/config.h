c78963a33ed2ef3a30138c244d9eeafd689c9f4f
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-02-18 12:23:18 +0000

    Logic shrimp final firmware

---
+++ trunk/LogicShrimp/firmware/config.h
@@ -0,0 +1,24 @@
+     #pragma config WDTEN = OFF       
+     #pragma config OSC = ECPLL    
+     #pragma config PLLDIV = 5           //Divide by 5 (20 MHz oscillator input) 
+     #pragma config STVREN = ON         
+     #pragma config XINST = OFF          
+     #pragma config CPUDIV = OSC1        
+     #pragma config CP0 = OFF               
+     #pragma config T1DIG = ON           
+     #pragma config LPT1OSC = OFF        
+     #pragma config FCMEN = OFF          
+     #pragma config IESO = OFF           
+     #pragma config WDTPS = 32768        
+     #pragma config DSWDTOSC = INTOSCREF 
+     #pragma config RTCOSC = T1OSCREF    
+     #pragma config DSBOREN = OFF        
+     #pragma config DSWDTEN = OFF       
+     #pragma config DSWDTPS = 8192       
+     #pragma config IOL1WAY = OFF        
+     #pragma config MSSP7B_EN = MSK7     
+     #pragma config WPFP = PAGE_1        
+     #pragma config WPEND = PAGE_0       
+     #pragma config WPCFG = OFF          
+     #pragma config WPDIS = OFF         
+     
