2524041f8145a7d7c0a2257026c8d31bc7eed8e7
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-02-23 08:07:52 +0000

    phpbb antispam - fixed load defaults too late.

--- trunk/phpBB3-MODs/disable-links-new-users/functions_link_filter.php
+++ trunk/phpBB3-MODs/disable-links-new-users/functions_link_filter.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* functions_link_filter.php version r725
+* functions_link_filter.php version r737
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 * Modified by Ian Lesnet (http://dangerousprototypes.com)
 * Documentation and install info here: 
