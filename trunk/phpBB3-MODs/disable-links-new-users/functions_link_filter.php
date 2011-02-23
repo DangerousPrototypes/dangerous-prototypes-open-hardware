56cf640366de86b88368712a92248cd76b22f51d
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-02-23 13:18:03 +0000

    moved user filter log to user log. Zombie registration purge is still in admin

--- trunk/phpBB3-MODs/disable-links-new-users/functions_link_filter.php
+++ trunk/phpBB3-MODs/disable-links-new-users/functions_link_filter.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* functions_link_filter.php version r740
+* functions_link_filter.php version r742
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 * Modified by Ian Lesnet (http://dangerousprototypes.com)
 * Documentation and install info here: 
@@ -92,7 +92,7 @@ class link_filter{
 		}else{
 			$l.='OK';
 		}
-		add_log('admin', 'LOG_SPAM_HAMMER', $l);
+		add_log('user', 'LOG_SPAM_HAMMER', $l);
 	}
 	
 /**
