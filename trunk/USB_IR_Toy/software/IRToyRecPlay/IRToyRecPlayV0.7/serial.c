1a9b3dd38d1fbb55596c3d20155b53bbf8205122
jamz@wherelabs.com <jamz@wherelabs.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-04-12 19:46:15 +0000

    added license headers and try to recompile, but serial.c has errors when it was replaced..

--- trunk/USB_IR_Toy/software/IRToyRecPlay/IRToyRecPlayV0.7/serial.c
+++ trunk/USB_IR_Toy/software/IRToyRecPlay/IRToyRecPlayV0.7/serial.c
@@ -1,25 +1,26 @@
 /*
- * This file is part of the Bus Pirate project (http://code.google.com/p/the-bus-pirate/).
- *
- * Written and maintained by the Bus Pirate project and http://dangerousprototypes.com
- *
- * To the extent possible under law, the project has
- * waived all copyright and related or neighboring rights to Bus Pirate. This
- * work is published from United States.
- *
- * For details see: http://creativecommons.org/publicdomain/zero/1.0/.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- */
-/*
- * OS independent serial interface
- *
- * Heavily based on Pirate-Loader:
+ * Part of ols-fwloader - Serial port routines
+ * Inspired by pirate-loader, written by Piotr Pawluczuk
  * http://the-bus-pirate.googlecode.com/svn/trunk/bootloader-v4/pirate-loader/source/pirate-loader.c
  *
- */
+ * Copyright (C) 2010 Piotr Pawluczuk
+ * Copyright (C) 2011 Michal Demin <michal.demin@gmail.com>
+ *
+ ********************************************************************************************************************************
+ *  Released into the public domain, 2011 Where Labs, LLC (DangerousPrototypes.com/Ian Lesnet)
+ *
+ *  This work is free: you can redistribute it and/or modify it under the terms of Creative Commons Zero license v1.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the License for more details. You should have received a copy of the License along with this program. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ *  Contact Details: http://www.DangerousPrototypes.com
+ *  Where Labs, LLC, 208 Pine Street, Muscatine, IA 52761,USA
+
+*/
+
+
+
+#include <config.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,108 +29,10 @@
 #include <string.h>
 
 #include "serial.h"
-extern int disable_comport;
-extern char *dumpfile;
-#ifdef _WIN32
-extern HANDLE dumphandle;
-#endif
-/*
-#ifdef WIN32
-	int write(int fd, const void* buf, int len)
-	{
-		HANDLE hCom = (HANDLE)fd;
-		int res = 0;
-		unsigned long bwritten = 0;
 
-
-		res = WriteFile(hCom, buf, len, &bwritten, NULL);
-
-		if( res == FALSE ) {
-			return -1;
-		} else {
-			return bwritten;
-		}
-	}
-
-	int read(int fd, void* buf, int len)
-	{
-		HANDLE hCom = (HANDLE)fd;
-		int res = 0;
-		unsigned long bread = 0;
-
-		res = ReadFile(hCom, buf, len, &bread, NULL);
-
-		if( res == FALSE ) {
-			return -1;
-		} else {
-			return bread;
-		}
-	}
-
-	int close(int fd)
-	{
-		HANDLE hCom = (HANDLE)fd;
-
-		CloseHandle(hCom);
-		return 0;
-	}
-
-	int open(const char* path, unsigned long flags)
-	{
-		static char full_path[32] = {0};
-
-		HANDLE hCom = NULL;
-
-		if( path[0] != '\\' ) {
-			_snprintf(full_path, sizeof(full_path) - 1, "\\\\.\\%s", path);
-			path = full_path;
-		}
-
-		hCom = CreateFileA(path, GENERIC_WRITE | GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-		if( !hCom || hCom == INVALID_HANDLE_VALUE ) {
-			return -1;
-		} else {
-			return (int)hCom;
-		}
-	}
-
-	int __stdcall select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfs, const struct timeval* timeout)
-	{
-		time_t maxtc = time(0) + (timeout->tv_sec);
-		COMSTAT cs = {0};
-		unsigned long dwErrors = 0;
-
-		if( readfds->fd_count != 1 ) {
-			return -1;
-		}
-
-		while( time(0) <= maxtc )
-		{ //only one file supported
-			if( ClearCommError( (HANDLE)readfds->fd_array[0], 0, &cs) != TRUE ){
-				return -1;
-			}
-
-			if( cs.cbInQue > 0 ) {
-				return 1;
-			}
-
-			Sleep(10);
-		}
-		return 0;
-	}
-	unsigned int sleep(unsigned int sec)
-	{
-		Sleep(sec * 1000);
-
-		return 0;
-	}
-#else
-#endif
-*/
-int serial_setup(int fd, speed_t speed)
+int serial_setup(int fd, unsigned long speed)
 {
-#ifdef WIN32
+#if IS_WIN32
 	COMMTIMEOUTS timeouts;
 	DCB dcb = {0};
 	HANDLE hCom = (HANDLE)fd;
@@ -144,6 +47,7 @@ int serial_setup(int fd, speed_t speed)
 	if( !SetCommState(hCom, &dcb) ){
 		return -1;
 	}
+
 	timeouts.ReadIntervalTimeout = 100;
 	timeouts.ReadTotalTimeoutMultiplier = 10;
 	timeouts.ReadTotalTimeoutConstant = 100;
@@ -157,12 +61,32 @@ int serial_setup(int fd, speed_t speed)
 	return 0;
 #else
 	struct termios t_opt;
+	speed_t baud;
+
+	switch (speed) {
+		case 921600:
+			baud = B921600;
+			break;
+		case 115200:
+			baud = B115200;
+			break;
+		case 1000000:
+			baud = B1000000;
+			break;
+		case 1500000:
+			baud = B1500000;
+			break;
+		default:
+			printf("unknown speed setting \n");
+			return -1;
+			break;
+	}
 
 	/* set the serial port parameters */
 	fcntl(fd, F_SETFL, 0);
 	tcgetattr(fd, &t_opt);
-	cfsetispeed(&t_opt, speed);
-	cfsetospeed(&t_opt, speed);
+	cfsetispeed(&t_opt, baud);
+	cfsetospeed(&t_opt, baud);
 	t_opt.c_cflag |= (CLOCAL | CREAD);
 	t_opt.c_cflag &= ~PARENB;
 	t_opt.c_cflag &= ~CSTOPB;
@@ -170,32 +94,35 @@ int serial_setup(int fd, speed_t speed)
 	t_opt.c_cflag |= CS8;
 	t_opt.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
 	t_opt.c_iflag &= ~(IXON | IXOFF | IXANY);
+	t_opt.c_iflag &= ~(ICRNL | INLCR);
+	t_opt.c_oflag &= ~(OCRNL | ONLCR);
 	t_opt.c_oflag &= ~OPOST;
 	t_opt.c_cc[VMIN] = 0;
 	t_opt.c_cc[VTIME] = 10;
-	tcflush(fd, TCIFLUSH);
-	tcsetattr(fd, TCSANOW, &t_opt);
+
+#if IS_DARWIN
+	if( tcsetattr(fd, TCSANOW, &t_opt) < 0 ) {
+		return -1;
+	}
+
+	return ioctl( fd, IOSSIOSPEED, &baud );
+#else
+	tcflush(fd, TCIOFLUSH);
+
+	return tcsetattr(fd, TCSANOW, &t_opt);
 #endif
-	return 0;
+#endif
 }
 
-int serial_write(int fd, char *buf, int size)
+int serial_write(int fd, const char *buf, int size)
 {
 	int ret = 0;
-#ifdef WIN32
+#if IS_WIN32
 	HANDLE hCom = (HANDLE)fd;
 	int res = 0;
 	unsigned long bwritten = 0;
 
-
 	res = WriteFile(hCom, buf, size, &bwritten, NULL);
-
-	//printf("Serial: Written %i bytes:  ",size);
-    //int i;
-    //for (i = 0; i < size; i++){
-		//printf("%02X ", buf[i]);
-	//}
-	//printf("\n");
 
 	if( res == FALSE ) {
 		ret = -1;
@@ -206,35 +133,29 @@ int serial_write(int fd, char *buf, int size)
 	ret = write(fd, buf, size);
 #endif
 
-	//fprintf(stderr, "size = %d ret = %d\n", size, ret);
-	//buspirate_print_buffer(buf, size);
+#if DEBUG
+	if (ret != size)
+		fprintf(stderr, "Error sending data (written %d should have written %d)\n", ret, size);
+#endif
 
-   	if (ret != size){
-	    	fprintf(stderr, " Error sending data\n");
-
-   	}
 	return ret;
-
-
 }
 
 int serial_read(int fd, char *buf, int size)
 {
-  	int len = 0;
+	int len = 0;
 	int ret = 0;
-#ifndef WIN32
 	int timeout = 0;
-#endif
-#ifdef WIN32
+#if IS_WIN32
 	HANDLE hCom = (HANDLE)fd;
 	unsigned long bread = 0;
 
 	ret = ReadFile(hCom, buf, size, &bread, NULL);
 
-	if( ret == FALSE || ret==-1 || bread<1) {
-		len= -1;
+	if( ret == FALSE || ret==-1 ) {
+		len = -1;
 	} else {
-		len=bread;
+		len = bread;
 	}
 
 #else
@@ -257,17 +178,19 @@ int serial_read(int fd, char *buf, int size)
 		len += ret;
 	}
 #endif
-	//printf("should have read = %i actual size = %i \n", size, len);
-	//fprintf(stderr, "should have read = %d actual size = %d \n", size, len);
-	//buspirate_print_buffer(buf, len);
 
-return len;
+#if DEBUG
+	if (len != size)
+		fprintf(stderr, "Error receiving data (read %d should have read %d)\n", len, size);
+#endif
+
+	return len;
 }
 
-int serial_open(char *port)
+int serial_open(const char *port)
 {
 	int fd;
-#ifdef WIN32
+#if IS_WIN32
 	static char full_path[32] = {0};
 
 	HANDLE hCom = NULL;
@@ -284,13 +207,10 @@ int serial_open(char *port)
 	} else {
 		fd = (int)hCom;
 	}
-
-
-
 #else
-	fd = open(port, O_RDWR | O_NOCTTY);
+	fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
 	if (fd == -1) {
-		fprintf(stderr, "Could not open serial port.");
+		//fprintf(stderr, "Could not open serial port.\n");
 		return -1;
 	}
 #endif
@@ -299,135 +219,13 @@ int serial_open(char *port)
 
 int serial_close(int fd)
 {
-#ifdef WIN32
+#if IS_WIN32
 	HANDLE hCom = (HANDLE)fd;
 
 	CloseHandle(hCom);
-
-	//close dumpfile if it was created
-
 #else
 	close(fd);
 #endif
 	return 0;
 }
 
-/*
-int readWithTimeout(int fd, uint8_t *out, int length, int timeout)
-{
-	fd_set fds;
-	struct timeval tv = {timeout, 0};
-	int res = -1;
-	int got = 0;
-
-	do {
-
-		FD_ZERO(&fds);
-		FD_SET(fd, &fds);
-
-		res = select(fd + 1, &fds, NULL, NULL, &tv);
-
-		if( res > 0 ) {
-			res = read(fd, out, length);
-			if( res > 0 ) {
-				length -= res;
-				got    += res;
-				out    += res;
-			} else {
-				break;
-			}
-		} else {
-			return res;
-		}
-	} while( length > 0);
-
-	return got;
-}
-
-int configurePort(int fd, unsigned long baudrate)
-{
-#ifdef WIN32
-	COMMTIMEOUTS timeouts;
-	DCB dcb = {0};
-	HANDLE hCom = (HANDLE)fd;
-
-	dcb.DCBlength = sizeof(dcb);
-
-	dcb.BaudRate = baudrate;
-	dcb.ByteSize = 8;
-	dcb.Parity = NOPARITY;
-	dcb.StopBits = ONESTOPBIT;
-
-	if( !SetCommState(hCom, &dcb) ){
-		return -1;
-	}
-
-
-	timeouts.ReadIntervalTimeout = 100;
-	timeouts.ReadTotalTimeoutMultiplier = 10;
-	timeouts.ReadTotalTimeoutConstant = 100;
-	timeouts.WriteTotalTimeoutMultiplier = 10;
-	timeouts.WriteTotalTimeoutConstant = 100;
-
-	if (!SetCommTimeouts(hCom, &timeouts)) {
-		return -1;
-	}
-
-	return (int)hCom;
-#else
-    speed_t baud = B921600;
-	struct termios g_new_tio;
-
-	switch (baudrate) {
-		case 921600:
-			baud = B921600;
-			break;
-		case 115200:
-			baud = B115200;
-			break;
-		case 1000000:
-			baud = B1000000;
-			break;
-		case 1500000:
-			baud = B1500000;
-		default:
-			printf("unknown speed setting \n");
-			return -1;
-			break;
-	}
-
-	memset(&g_new_tio, 0x00 , sizeof(g_new_tio));
-	cfmakeraw(&g_new_tio);
-
-	g_new_tio.c_cflag |=  (CS8 | CLOCAL | CREAD);
-	g_new_tio.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
-	g_new_tio.c_oflag = 0;
-	g_new_tio.c_lflag = 0;
-
-	g_new_tio.c_cc[VTIME] = 0;
-	g_new_tio.c_cc[VMIN] = 1;
-
-#ifdef MACOSX
-
-	if( tcsetattr(fd, TCSANOW, &g_new_tio) < 0 ) {
-		return -1;
-	}
-
-	return ioctl( fd, IOSSIOSPEED, &baud );
-#else
-	cfsetispeed (&g_new_tio, baudrate);
-	cfsetospeed (&g_new_tio, baudrate);
-
-	tcflush(fd, TCIOFLUSH);
-
-	return tcsetattr(fd, TCSANOW, &g_new_tio);
-#endif //#ifdef MACOSX
-
-#endif
-}
-
-int openPort(const char* dev, unsigned long flags)
-{
-	return open(dev, O_RDWR | O_NOCTTY | O_NDELAY | flags);
-}
-*/
--- trunk/USB_IR_Toy/software/IRToyRecPlay/IRToyRecPlayV0.7/serial.h
+++ trunk/USB_IR_Toy/software/IRToyRecPlay/IRToyRecPlayV0.7/serial.h
@@ -1,67 +1,30 @@
 /*
- * This file is part of the Bus Pirate project (http://code.google.com/p/the-bus-pirate/).
- *
- * Written and maintained by the Bus Pirate project and http://dangerousprototypes.com
- *
- * To the extent possible under law, the project has
- * waived all copyright and related or neighboring rights to Bus Pirate. This
- * work is published from United States.
- *
- * For details see: http://creativecommons.org/publicdomain/zero/1.0/.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- */
-/*
- * OS independent serial interface
- *
- * Heavily based on Pirate-Loader:
+ * Part of ols-fwloader - Serial port routines
+ * Inspired by pirate-loader, written by Piotr Pawluczuk
  * http://the-bus-pirate.googlecode.com/svn/trunk/bootloader-v4/pirate-loader/source/pirate-loader.c
  *
- */
-#ifndef MYSERIAL_H_
-#define MYSERIAL_H_
-/*
-#ifdef WIN32
-	#include <windows.h>
-	#include <time.h>
+ * Copyright (C) 2010 Piotr Pawluczuk
+ * Copyright (C) 2011 Michal Demin <michal.demin@gmail.com>
+ *
+ ********************************************************************************************************************************
+ *  Released into the public domain, 2011 Where Labs, LLC (DangerousPrototypes.com/Ian Lesnet)
+ *
+ *  This work is free: you can redistribute it and/or modify it under the terms of Creative Commons Zero license v1.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the License for more details. You should have received a copy of the License along with this program. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ *  Contact Details: http://www.DangerousPrototypes.com
+ *  Where Labs, LLC, 208 Pine Street, Muscatine, IA 52761,USA
 
-	#define O_NOCTTY 0
-	#define O_NDELAY 0
-	#define B115200 115200
-	#define B921600 921600
-
-	#define OS WINDOWS
-
-    int write(int fd, const void* buf, int len);
-    int read(int fd, void* buf, int len);
-    int close(int fd);
-    int open(const char* path, unsigned long flags);
-    int __stdcall select(int nfds, fd_set* readfds, fd_set* writefds, fd_set* exceptfs, const struct timeval* timeout);
-
-#else
-	#include <unistd.h>
-	#include <termios.h>
-	#include <sys/select.h>
-	#include <sys/types.h>
-	#include <sys/time.h>
-
-	#ifdef MACOSX
-		#include <IOKit/serial/ioss.h>
-		#include <sys/ioctl.h>
-
-		#define B1500000 1500000
-		#define B1000000 1000000
-		#define B921600  921600
-	#endif
-
-#endif
 */
 
+#ifndef MYSERIAL_H_
+#define MYSERIAL_H_
+
+#include <config.h>
 #include <stdint.h>
 
-#ifdef WIN32
+#if IS_WIN32
 #include <windows.h>
 #include <time.h>
 
@@ -69,7 +32,7 @@
 #define B921600 921600
 
 typedef long speed_t;
-#else
+#else // WIN32
 
 #include <unistd.h>
 #include <termios.h>
@@ -79,10 +42,19 @@ typedef long speed_t;
 
 #endif
 
-int serial_setup(int fd, speed_t speed);
-int serial_write(int fd, char *buf, int size);
+#if IS_DARWIN
+#include <IOKit/serial/ioss.h>
+#include <sys/ioctl.h>
+
+#define B1500000 1500000
+#define B1000000 1000000
+#define B921600  921600
+#endif
+
+int serial_setup(int fd, unsigned long speed);
+int serial_write(int fd, const char *buf, int size);
 int serial_read(int fd, char *buf, int size);
-int serial_open(char *port);
+int serial_open(const char *port);
 int serial_close(int fd);
 
 
