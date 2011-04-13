2cfbf1f41d00fd58e288522880dadaa111bb60bf
ianlesnet <ianlesnet@gmail.com>
2011-04-13 10:23:06 +0000

    schematic 2 tutorial

---
+++ CPLD_Breakout/package/Tutorials/xc9572xl/Schematic-CPLDIntro2Ledbutton/_impact.cmd
@@ -0,0 +1,21 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port xsvf -file "E:/Work/dp/CPLD_Breakout/package/Tutorials/xc9572xl/Schematic-CPLDIntro2Ledbutton/sch-CPLDIntro2Ledbutton.xsvf"
+addDevice -p 1 -file "E:/Work/dp/CPLD_Breakout/package/Tutorials/xc9572xl/Schematic-CPLDIntro2Ledbutton/CPLDIntro2Ledbutton.jed"
+Program -p 1 -e -v 
+setCable -port svf -file "E:/Work/dp/CPLD_Breakout/package/Tutorials/xc9572xl/Schematic-CPLDIntro2Ledbutton/sch-CPLDIntro2Ledbutton.svf"
+Program -p 1 -e -v 
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+saveProjectFile -file "\auto_project.ipf"
+deleteDevice -position 1
