07c9875ae3412eb1bc1217eba6f18bc51f20307e
jamz <jamz@wherelabs.com>
2011-05-12 19:38:48 +0000

    updated batch file to include -b parameter

--- USB_IR_Toy/package/software/IRToyRecPlay/run.bat
+++ USB_IR_Toy/package/software/IRToyRecPlay/run.bat
@@ -1,2 +1,8 @@
-irtoy -d COM10 -p -f tv  -v -r
+rem  -b 256 is playback buffer for finetuning in your system. default is 246 when not specfied.
+rem  to play single file use the full file name with extenstion .bin
+rem irtoy -d COM10 -p -f test_001.bin -v -b 256
+
+
+irtoy -d COM10 -p -f test -v -r -b 256
 pause
+
