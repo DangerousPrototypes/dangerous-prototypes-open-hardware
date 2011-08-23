2d505ce50f3f7975dfadc591d2e19d1ed8288da1
jjlsantos@gmail.com <jjlsantos@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-08-23 10:26:04 +0000

    remove comments

--- trunk/art/announce/banner.sql
+++ trunk/art/announce/banner.sql
@@ -7,27 +7,7 @@
 -- Server version: 5.1.49
 -- PHP Version: 5.3.3-7+squeeze1
 
-/*
-       Simple announcement banner rotator with impression and click tracking.
 
-   Used for site-wide announcements at DangerousPrototypes.com
-   Copyright (C) 2011 Where Labs, LLC (http://dangerousprototypes.com)
-   Written by Vyacheslav V Sokolov
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see<http://www.gnu.org/licenses/>.
-
-*/
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
