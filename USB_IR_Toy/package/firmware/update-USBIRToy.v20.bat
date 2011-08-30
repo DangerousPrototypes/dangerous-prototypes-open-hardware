2d88a902e4fcd4df9cef824ac54e29daa17dfceb
ianlesnet <ianlesnet@gmail.com>
2011-08-30 13:23:02 +0000

    Added Dukey's bootlaoder helper: http://dangerousprototypes.com/forum/viewtopic.php?f=29&t=2721&view=unread#p26580

--- USB_IR_Toy/package/firmware/update-USBIRToy.v20.bat
+++ USB_IR_Toy/package/firmware/update-USBIRToy.v20.bat
@@ -1,2 +1,10 @@
+@ECHO OFF
+set /P INPUT=Enter Com Port Number %=%
+
+BootLoader.exe %INPUT%
+
+ECHO Once the device has entered boot loader mode
+pause
+
 fw_update -e -w -v -m flash -vid 0x04D8 -pid 0xFD0B -ix USBIRToy.v20.hex
 pause
