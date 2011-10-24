b3dd0192d7ce47665276d26dc257deb059321858
ianlesnet <ianlesnet@gmail.com>
2011-10-24 06:52:42 +0000

    flash destroyer batch file note update from here: http://dangerousprototypes.com/forum/viewtopic.php?f=36&t=3010&view=unread#p29429

--- Flash_Destroyer/firmware/update-FD-firmware-v1.bat
+++ Flash_Destroyer/firmware/update-FD-firmware-v1.bat
@@ -1,2 +1,4 @@
+echo Note: To make the Flash Destroyer act as a USB device in order to update firmware, you must hold the button down, apply power, THEN hook up the USB cable. Otherwise, it'll not work.
+pause
 fw_update -e -w -v -m flash -vid 0x04D8 -pid 0xFBEB -ix FD-firmware-v1.hex
 pause
