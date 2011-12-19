4eb39263fb61a62ebe1b334442d3797c8442e8ad
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-12-19 12:07:19 +0000

    bp demo v5 SPI update frm Sjaak

--- trunk/Breakout_Boards/Bus_Pirate_demo/Firmware/spi.c
+++ trunk/Breakout_Boards/Bus_Pirate_demo/Firmware/spi.c
@@ -1,13 +1,29 @@
 /* header */
 
+#include <pic.h>
 #include "./spi.h"
 
 void initSPI(void)
 {
+   // setup SPI slave
+    SSP2STAT=0x40;      // !smp, cke
+    SSP2CON1=0x24;      // SSP1EN, !CKP, SSPM5 (SPI slave, cc enabled)
+    SSP2CON3=0x00;      // !BOEN
+
+    SSP2BUF=0xAA;       // intial byte te send to master
 
 }
 
 void SPIworker(void)
 {
+	unsigned char temp;
 
+    while(1)
+    {
+        if(SSP2STATbits.BF)	// we received something :)
+        {
+            temp=SSP2BUF;
+            SSP2BUF=temp;   //echo back
+        }
+    }
 }
