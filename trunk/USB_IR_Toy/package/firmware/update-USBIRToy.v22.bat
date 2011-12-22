1dd17a87506b20c1694a00c73becf3e3112fe4e8
ianlesnet@gmail.com <ianlesnet@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2011-12-22 12:40:21 +0000

    IR Toy firmware v22

---
+++ trunk/USB_IR_Toy/package/firmware/update-USBIRToy.v22.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+set /P INPUT=Enter Com Port Number %=%
+
+BootLoader.exe %INPUT%
+
+ECHO Once the device has entered boot loader mode
+pause
+
+fw_update -e -w -v -m flash -vid 0x04D8 -pid 0xFD0B -ix USBIRToy.v22.hex
+pause
