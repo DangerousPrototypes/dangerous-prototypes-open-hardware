0b3f35da803079036c2990adf4531f5eedb10977
sjaak2002 <sjaak2002@msn.com>
2012-01-18 19:14:52 +0000

    initial version of the freestanding USB stack, based on the revision 1599 of echo-demo

---
+++ USB_stack/echo-demo_v2/HardwareProfile.h
@@ -0,0 +1,10 @@
+/*
+
+ hardwareprofile
+
+ */
+
+
+#define _XTAL_FREQ	48000000 // 16MHz
+
+
---
+++ USB_stack/echo-demo_v2/echo-demo.X/build/default/production/_ext/1329223797/usb_stack.o.d
@@ -0,0 +1,10 @@
+build/default/production/_ext/1329223797/usb_stack.o : \
+	/home/chris/svn/dangerous-prototypes-open-hardware/USB_stack/common/usb_stack.c \
+	/home/chris/svn/dangerous-prototypes-open-hardware/USB_stack/common/usb_stack.h \
+	/opt/microchip/mplabc18/v3.40/h/GenericTypeDefs.h \
+	/opt/microchip/mplabc18/v3.40/h/stddef.h \
+	/home/chris/svn/dangerous-prototypes-open-hardware/USB_stack/echo-demo_v2/usb_config.h \
+	/home/chris/svn/dangerous-prototypes-open-hardware/USB_stack/common/picusb.h \
+	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h \
+	/opt/microchip/mplabc18/v3.40/h/p18f24j50.h \
+
