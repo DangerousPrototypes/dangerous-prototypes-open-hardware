e5b07c2687ff1794f88a702d6361983ad9002ea6
filip.dulic <filip.dulic@gmail.com>
2012-03-01 14:49:10 +0000

    project rebuild, and hex file for the PIC32DIP self test

--- Microcontrollers/PIC32_DIP/Firmware/PIC32DIP_SelfTest/nbproject/Package-default.bash
+++ Microcontrollers/PIC32_DIP/Firmware/PIC32DIP_SelfTest/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PIC32DIP_SelfTest.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=PIC32DIP_SelfTest.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=pic32dipselftest.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/PIC32DIP_SelfTest.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=PIC32DIP_SelfTest.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=pic32dipselftest/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/pic32dipselftest.x/bin
+makeDirectory ${TMPDIR}/pic32dipselftest/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/pic32dipselftest.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/pic32dipselftest.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pic32dipselftest.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/pic32dipselftest.tar *
 checkReturnCode
 
 # Cleanup
