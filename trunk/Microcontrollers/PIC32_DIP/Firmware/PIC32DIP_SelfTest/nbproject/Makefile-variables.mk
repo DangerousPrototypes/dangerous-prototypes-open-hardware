a0f748138eaf066d4b132d071f5c6380b259f5d8
filip.dulic@gmail.com <filip.dulic@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2012-03-01 14:49:10 +0000

    project rebuild, and hex file for the PIC32DIP self test

--- trunk/Microcontrollers/PIC32_DIP/Firmware/PIC32DIP_SelfTest/nbproject/Makefile-variables.mk
+++ trunk/Microcontrollers/PIC32_DIP/Firmware/PIC32DIP_SelfTest/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=PIC32DIP_SelfTest.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/PIC32DIP_SelfTest.X.production.hex
+CND_ARTIFACT_NAME_default=PIC32DIP_SelfTest.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PIC32DIP_SelfTest.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=pic32dipselftest.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic32dipselftest.x.tar
+CND_PACKAGE_NAME_default=pic32dipselftest.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic32dipselftest.tar
