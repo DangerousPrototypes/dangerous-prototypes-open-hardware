650974128deeab525b13471671cee2cbd7ab4254
filip.dulic@gmail.com <filip.dulic@gmail.com@0af3c9b6-adb7-11de-b66c-2b8a8bb3ca59>
2012-05-25 15:03:45 +0000

    Part Ninja Firmware early alpha testing releas

---
+++ trunk/Part-Ninja/firmware/main.c
@@ -0,0 +1,95 @@
+#include "globals.h"
+#include "config.h"
+#include "HD44780.h"
+#include "test.h"
+
+static void init(void);
+
+
+
+
+#pragma code
+void main(void)
+{  
+    init();			//setup the crystal, pins
+
+	Delay_MS(10);	
+
+	HD44780_Reset();//setup the LCD
+	HD44780_Init();
+	//LCD_Backlight(1);//turn it on, we ignore the parameter
+
+	LCD_CursorPosition(0);
+	LCD_WriteString("Part Ninja v0.0a");
+	LCD_CursorPosition(21);
+	LCD_WriteString("      testing...");
+ 
+	while(1) test();
+}//end main
+
+static void init(void){
+	unsigned int cnt = 2048;
+	
+	//all pins digital
+	//disable some defaults
+    ADCON1 |= 0b1111;   	//all pins digital
+	CVRCON=0b00000000;
+
+	//make sure everything is input (should be on startup, but just in case)
+	TRISA=0xff;
+	TRISB=0xff;
+	TRISC=0b11111111; 
+
+	//comparator analog pins setup...
+/*	CMCONbits.C2INV = 1;	//whne voltage on RA1 > vreff cout2=>1
+	CMCONbits.C1INV = 1;	//whne voltage on RA1 > vreff cout1=>1
+	CMCONbits.CIS=0; 		//the Cvin- input aer RA0 na RA1
+	CMCONbits.CM2=1,CMCONbits.CM1=0,CMCONbits.CM0=0; //100 C1 and C2 V+ conencted to vref, while v+ conected to RA1 and RA2
+	INTCONbits.GIE = 1;
+	INTCONbits.PEIE =1;
+	IPR2bits.CMIP =1;
+
+	//Timer 1 contorl setup
+	T1CONbits.RD16=1;
+	T1CONbits.T1RUN=0;
+	T1CONbits.T1CKPS=3;//8prescaler
+	T1CONbits.TMR1CS=0;*/
+		//on 18f24j50 we must manually enable PLL and wait at least 2ms for a lock
+	//OSCTUNEbits.PLLEN = 1;  //enable PLL
+	//while(cnt--); //wait for lock
+
+}
+
+#pragma interrupt high_isr
+
+void    high_isr(void)
+{
+   /* high priority interrupt handling code here */
+	/*
+	T1CONbits.TMR1ON =0;
+	CMP_INTE=0;
+	testCMP=1;**/
+}
+
+#pragma interruptlow low_isr
+
+void    low_isr(void)
+{
+   /* low priority interrupt handling code here */
+
+}
+
+#pragma code high_vector=0x08
+
+void    high_vector(void)
+{
+   _asm GOTO high_isr _endasm
+}
+
+#pragma code low_vector=0x18
+
+void    low_vector(void)
+{
+   _asm GOTO low_isr _endasm
+}
+
---
+++ trunk/Part-Ninja/firmware/test.h
@@ -0,0 +1,120 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "globals.h"
+
+
+#define LOW 0
+#define HIGH 1
+
+//////TESTING CONSTANTS/////////////////
+///for easier tunning purpses//////////
+//if the voltage drops by 100 high side read will be 923,low side will be 100
+#define CP_HIGH 	900	//V drop for HIGH side conductivity test
+#define CP_LOW  	123	//V drop for LOW side conductivity test
+
+#define CAP_DIFF 	10	//by how much will a CAP charge over noise...
+#define RES_DD 		170	//by how much will zener Vdrop change with diff resistors
+#define ZENER_DD 	100	//by how much will 2 anti paraller diodes VDs differ 
+#define FET_HIGH    923
+#define FET_LOW     100
+#define TST_DEL    	100
+
+
+//Enum with the different part type names...
+//CC is a common cathode double diode
+//CA is a common anode -||-
+typedef enum type
+{ERROR=0,NFET,PFET,NMOS,PMOS,NPN,CA,PNP,CC,
+	DIODE,TRIAC,SCR,CAP,RES,ZENER,DD,NOID=20}type;
+
+void compInit(u8 Pin);
+
+void initT();
+
+//Test function...does the testing..encapsulates the whole testing process for main...
+u8 test();
+//Checks conductivity through form [A]->[B] while keeping the 3rd pin at the test state birefly before the test.
+//5V--[680]-Vt-[A]-->[B]---GND, if Vt < 93% VCC it is conducting
+u16 checkConduct(u8 A, u8 B,u8 test);
+//runs twice through all 6 posile conductive paths between 3pins
+//once while keeping the 3rd pin LOW briefly befor the test
+//second while HIGH, the pin is at HiZ during the test 
+u8 testConduct(u8*diff);
+
+
+//called only if nC =0, so no CP were found in the r680 test...
+u8 testCAP_RES();
+//get Part SubSection other then Res/Cap/DD/Zener
+type getPartSS(u8 diff);
+//get Part SubSection if Res/Cap/DD/Zener
+type getPartSS2();
+//main called for further annalysis based on part subsection
+u8 switchPart(type SS);
+
+//These functions test the mosfet parts
+//they return the Ron value in ohms,
+//or 0 if the part is not a mosfet
+u16 testNFET();
+u16 testPFET();
+u16 testNMOS();
+u16 testPMOS();
+u16 testNPN();
+u16 testCA();
+u16 testPNP();
+u16 testCC();
+u16 testDIODE();
+u16 testTRIAC();
+u16 testSCR();
+u16 testCAP();
+u16 testRES();
+u16 testZENER();
+u16 testDD();
+
+
+
+//stes up the ADC module and takes a reading of the Pin
+u16 ReadADC(u8 Pin);
+//only sets up the ADC, for future fast acquisitions
+void quickADCsetup(u8 Pin);
+
+void Delay_MS(u8 s);
+
+//conects the pin to state(HIGH/LOW) through a 0,680,or 470k resistor
+void R_680 (u8 Pin, u8 State);
+void R_470K (u8 Pin, u8 State);
+void R_0 (u8 Pin, u8 State);
+//Brings the Pin into high impedance mode(input)
+void HiZ(u8 Pin);
+//Brings pins A,B, and C into HiZ
+void HiZ3(u8 A,u8 B, u8 C);
+
+//same as above functions, only it initialises the quick ADC
+//and uses the delay once the pin is set before teh ADC is read..
+
+u16 R_680adc (u8 Pin, u8 State,u16 delay);
+u16 R_470Kadc (u8 Pin, u8 State,u16 delay);
+//reads the ADCpin when the Pin is brought to HiZ, if delay=0--no delay..
+u16 HiZadc(u8 Pin,u8 ADCpin, u16 delay);
+
+
+
+
+
+////////////////////////////////////////////////////
+//Global Variables//////////////////////////////////
+
+//The list where all the conductive paths are kept.
+//[0]->[1] pins for direction, [3] is the read value r680
+//5v--[680]--[0]-->[1]---GND
+extern u16 tList[12][3];
+extern u8 nC;
+extern char pins[3];
+extern u8 tIN[3];
+extern u8 tOUT[3];
+extern u8 tC[3];
+extern u8 tN[8];
+extern u8 node; 
+extern u8 testCMP;
+extern u8 CMPpin;
+#endif
