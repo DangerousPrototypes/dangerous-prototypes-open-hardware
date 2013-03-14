2f4c6656602f36082875e93c155c4605783356f2
lemuix <lemuix@fake-url.com>
2013-03-14 04:09:54 +0000

    I know the SPI module works with the changes made to the underlying communication tools. However, all the other modules need some verification. I'll probably try to put together some unit-tests. However, at the moment, I actually don't have a bus-pirate on hand, so it may be a week or two.

--- pyBusPirateLite/BitBang.py
+++ pyBusPirateLite/BitBang.py
@@ -22,10 +22,13 @@ along with pyBusPirate.  If not, see <http://www.gnu.org/licenses/>.
 
 import select
 import serial
+import sys
+import time
 
 """
 PICSPEED = 24MHZ / 16MIPS
 """
+
 
 class PinCfg:
 	POWER = 0x8
@@ -33,159 +36,286 @@ class PinCfg:
 	AUX = 0x2
 	CS = 0x1
 
+
 class BBIOPins:
 	# Bits are assigned as such:
-	MOSI = 0x01;
-	CLK = 0x02;
-	MISO = 0x04;
-	CS = 0x08;
-	AUX = 0x10;
-	PULLUP = 0x20;
-	POWER = 0x40;
+	MOSI = 0x01
+	CLK = 0x02
+	MISO = 0x04
+	CS = 0x08
+	AUX = 0x10
+	PULLUP = 0x20
+	POWER = 0x40
+
 
 class BBIO:
 	def __init__(self, p="/dev/bus_pirate", s=115200, t=1):
 		self.port = serial.Serial(p, s, timeout=t)
-	
+
 	def BBmode(self):
-		self.port.flushInput();		
-		for i in range(20):
-			self.port.write("\x00");
-			r,w,e = select.select([self.port], [], [], 0.01);
-			if (r): break;
-		if self.response(5) == "BBIO1": return 1
-		else: return 0
+		if sys.platform == 'win32':
+			# I haven't seen this problem on Windows, which is good,
+			# because it doesn't appear to support select.select()
+			# for serial ports.  This seems to work instead:
+			self.resetBP()
+			self.port.write("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+			self.timeout(0.1)
+			self.port.flushInput()
+			self.reset()
+		else:
+			# Sometimes sending 20 zeroes in a row to a BP already in binary mode
+			# leads to BP getting stuck in BitBang mode
+			# (see forum: http://dangerousprototypes.com/forum/viewtopic.php?t=1440#msg13179 )
+			# so if we detect a response (indicating we're in BitBang mode before the 20 zeroes)
+			# stop sending zeroes and go on.
+			self.port.flushInput()
+			for i in range(20):
+				self.port.write("\x00")
+				r, w, e = select.select([self.port], [], [], 0.01)
+				if (r):
+					break
+
+		if self.response(5) == "BBIO1":
+			return 1
+		else:
+			return 0
 
 	def reset(self):
 		self.port.write("\x00")
-		self.timeout(0.1)
+		time.sleep(0.1)
 
 	def enter_SPI(self):
-		self.response(5)
+		self.response(5, errOnTimout=False)
 		self.port.write("\x01")
-		self.timeout(0.1)
-		if self.response(4) == "SPI1": return 1
-		else: return 0
+
+		if self.response(4) == "SPI1":
+			return 1
+		else:
+			return 0
 
 	def enter_I2C(self):
 		self.port.write("\x02")
-		self.timeout(0.1)
-		if self.response(4) == "I2C1": return 1
-		else: return 0
+
+		if self.response(4) == "I2C1":
+			return 1
+		else:
+			return 0
 
 	def enter_UART(self):
 		self.port.write("\x03")
-		self.timeout(0.1)
-		if self.response(4) == "ART1": return 1
-		else: return 0
-		
+
+		if self.response(4) == "ART1":
+			return 1
+		else:
+			return 0
+
 	def enter_1wire(self):
 		self.port.write("\x04")
-		self.timeout(0.1)
-		if self.response(4) == "1W01": return 1
-		else: return 0
-		
+
+		if self.response(4) == "1W01":
+			return 1
+		else:
+			return 0
+
 	def enter_rawwire(self):
 		self.port.write("\x05")
-		self.timeout(0.1)
-		if self.response(4) == "RAW1": return 1
-		else: return 0
-		
+
+		if self.response(4) == "RAW1":
+			return 1
+		else:
+			return 0
+
 	def resetBP(self):
 		self.reset()
 		self.port.write("\x0F")
-		self.timeout(0.1)
+
 		#self.port.read(2000)
 		self.port.flushInput()
 		return 1
 
 	def raw_cfg_pins(self, config):
 		self.port.write(chr(0x40 | config))
-		self.timeout(0.1)
+
 		return self.response(1)
 
 	def raw_set_pins(self, pins):
-		self.port.write(chr(0x80 | config))
-		self.timeout(0.1)
+		self.port.write(chr(0x80 | pins))
+
 		return self.response(1)
 
-	def timeout(self, timeout=0.1):
-		select.select([], [], [], timeout)
 
-	def response(self, byte_count=1, return_data=False):
+	def response(self, byte_count=1, return_data=False, timeout=0.1, errOnTimout=True):		# This is basically the primary communication function.
+													# The timeout function is new, and the other tools (aside from the SPI library, which has been tested pretty thoroughly)
+													# will need to be tested
+
+													# Adding the ability to receive an unknown number of bytes would be a very good idea in the near future
+
+		startTime = time.time()
+
+		while (self.port.inWaiting() < byte_count) and (time.time() < (startTime + timeout)):		# Wait until we have the desired number of
+			time.sleep(0.001)									# bytes in the receive buffer
+
+		if time.time() > (startTime + timeout) and errOnTimout:
+			raise IOError("Response Timed out")
+
 		data = self.port.read(byte_count)
-		if byte_count == 1 and return_data == False:
-			if data == chr(0x01): return 1
-			else: return 0
+
+		if byte_count == 1 and return_data is False:				# If you don't request anyt return data, the bus-pirate returns 0x01 upon success
+			if data == chr(0x01):
+				return 1
+			else:
+				return 0
 		else:
 			return data
 
 	""" Self-Test """
 	def short_selftest(self):
 		self.port.write("\x10")
-		self.timeout(0.1)
+
 		return self.response(1, True)
 
 	def long_selftest(self):
 		self.port.write("\x11")
-		self.timeout(0.1)
+
 		return self.response(1, True)
 
 	""" PWM """
 	def setup_PWM(self, prescaler, dutycycle, period):
 		self.port.write("\x12")
-		self.port.write(prescaler)
-		self.port.write((dutycycle>>8)&0xFF)
-		self.port.write(dutycycle&0xFF)
-		self.port.write((period>>8)&0xFF)
-		self.port.write(period&0xFF)
-		self.timeout(0.1)
+		self.port.write(chr(prescaler))
+		self.port.write(chr((dutycycle >> 8) & 0xFF))
+		self.port.write(chr(dutycycle & 0xFF))
+		self.port.write(chr((period >> 8) & 0xFF))
+		self.port.write(chr(period & 0xFF))
+
 		return self.response()
+
+	# Sets the pwm frequency in 1khz multiples as the BusPirate shell/firmware does
+	def set_1khz_pwm_frequency(self, kilohertz, duty_cycle = .5):
+		if kilohertz < 4:
+			pwm_div = 62
+			prescaler = 3
+
+		elif kilohertz < 31:
+			pwm_div = 250
+			prescaler = 2
+		elif kilohertz < 245:
+			pwm_div = 2000
+			prescaler = 1
+		else:
+			pwm_div = 16000
+			prescaler = 0
+
+		period = (pwm_div / kilohertz) - 1
+
+		duty = int(period * duty_cycle)
+
+		#print "prescaler ", prescaler, "duty ", duty, "period ", period
+
+		return self.setup_PWM(prescaler, duty, period)
+
+	# Sets the pwm aux pin at an specific frequency and duty cycle
+	#
+	# The code follows the PIC24FJ64 datasheet specification an the
+	# PIC24F family reference manual.
+	#
+	# Basically it finds the highest period register value for the timer
+	# based on our target frequency or target period.
+	#
+	# The timer input clock is rated at Fosc/2 and it's called on the
+	# datasheets as Tcy, the BusPirate uses the internal 8Mhz clock with
+	# the PLL module enabled, so finally it uses a 32Mhz clock, Tcy
+	# is 16Mhz or a 62.5ns period.
+
+	def set_pwm_frequency(self, frequency, duty_cycle = 0.5):
+
+		prescalers = [1.0, 1.0 / 8, 1.0 / 64, 1.0 / 256]
+
+		# Timer input frequency = Fosc/2 --> 16000000Hz
+		# Timer period with 1:1 prescaler = 1/16000000 --> 0.0000000625s --> 0.0625us --> 62.5ns
+
+		tcy = 0.0000000625
+
+		target_period = 1.0 / frequency
+
+		# precalc some part of the period register formula
+		dummy = target_period / tcy
+
+		prescaler = -1
+
+		for I in range(4):
+			period = dummy * prescalers[I] - 1
+
+			# It's a valid 16 bits period register value?
+			if period < 65536:
+				# If so we have found the greatest one
+				prescaler = I
+				break
+
+		# If we have found a valid prescaler, calc the duty cycle value
+		# and call the BusPirate function for stablishing the PWM configuration
+
+		if prescaler != -1:
+			duty = int(period * duty_cycle)
+			#print "prescaler ", prescaler, " period ", period, "duty", duty
+			return self.setup_PWM(prescaler, duty, int(period))
+		else:
+			print "Can't setup ", frequency, "Hz."
+
 
 	def clear_PWM(self):
 		self.port.write("\x13")
-		self.timeout(0.1)
+
 		return self.response()
 
-	""" ADC """	
+	""" ADC """
 	def ADC_measure(self):
 		self.port.write("\x14")
-		self.timeout(0.1)
+
 		return self.response(2, True)
 
 	""" General Commands for Higher-Level Modes """
 	def mode_string(self):
 		self.port.write("\x01")
-		self.timeout(0.1)
+
 		return self.response()
 
-	def bulk_trans(self, byte_count=1, byte_string=None):
-		if byte_string == None: pass
-		self.port.write(chr(0x10 | (byte_count-1)))
-		#self.timeout(0.1)
-		for i in range(byte_count):
-			self.port.write(chr(byte_string[i]))
-			#self.timeout(0.1)
-		data = self.response(byte_count+1, True)
+	def bulk_trans(self, input):
+		if type(input) == int:
+			byteList = [0] * input
+			byteCount = input
+		elif type(input) == list:
+			byteList = input
+			byteCount = len(input)
+		else:
+			raise ValueError("Invalid data type")
+
+
+		self.port.write(chr(0x10 | (byteCount - 1)))
+
+		for i in range(byteCount):
+			self.port.write(chr(byteList[i]))
+
+		data = self.response(byteCount + 1, True)
 		return data[1:]
 
 	def cfg_pins(self, pins=0):
 		self.port.write(chr(0x40 | pins))
-		self.timeout(0.1)
+
 		return self.response()
 
 	def read_pins(self):
 		self.port.write("\x50")
-		self.timeout(0.1)
+
 		return self.response(1, True)
 
 	def set_speed(self, spi_speed=0):
 		self.port.write(chr(0x60 | spi_speed))
-		self.timeout(0.1)
+
 		return self.response()
 
 	def read_speed(self):
 		self.port.write("\x70")
-		select.select(None, None, None, 0.1)
+
 		return self.response(1, True)
 
--- pyBusPirateLite/RAW_WIRE.py
+++ pyBusPirateLite/RAW_WIRE.py
@@ -31,11 +31,11 @@ class RAW_WIRE_OUT_TYPE:
 class RAW_WIRE_WIRES_TYPE:
 	TWO = 0
 	THREE = 1
-	
+
 class RAW_WIRE_BIT_ORDER_TYPE:
 	MSB = 0
 	LSB = 1
-	
+
 class RAW_WIRE_COMMANDS:
 	RESET 		= "\x00"
 	VERSION 	= "\x01"
@@ -51,111 +51,112 @@ class RAW_WIRE_COMMANDS:
 	CLK_HIGH	= "\x0B"
 	DATA_LOW	= "\x0C"
 	DATA_HIGH	= "\x0D"
-	BULK_TRANS	= "\x10" # implemented as general commands on BitBang.py
+	BULK_TRANS	= "\x10"		# implemented as general commands on BitBang.py
 	BULK_CLK	= "\x20"
 	CFG_PERIPHERALS = "\x40"
 	SET_SPEED	= "\x60"
 	CFG_MODE	= "\x80"
-	
+
 
 class RAW_WIRE(BBIO):
 	bulk_read = None
+
 	def __init__(self, port, speed):
 		BBIO.__init__(self, port, speed)
-		
+
 	#def enter_mode(self):
 	#	if self.BBmode() && self.enter_raw_wire(): return True
 	#	else: return False
 	#	    if not rw.BBmode():
-		
+
 	def command(self, command, response_length, timeout=0.1):
 		self.port.write(command)
 		self.timeout(timeout)
 		return self.response(response_length, True)
-		
+
 	# 00000000 – Enter raw bitbang mode, reset to raw bitbang mode
 	# implement it?
-	
+
 	# 00000001 – Mode version string (RAW1)
-	
+
 	def version(self):
-		return self.command(RAW_WIRE_COMMANDS.VERSION,4)
-		
+		return self.command(RAW_WIRE_COMMANDS.VERSION, 4)
+
 	# 0000001x – I2C-style start (0) / stop (1) bit
-	
+
 	def I2C_Start(self):
-		return self.command(RAW_WIRE_COMMANDS.I2C_START,1)
-	
+		return self.command(RAW_WIRE_COMMANDS.I2C_START, 1)
+
 	def I2C_Stop(self):
-		return self.command(RAW_WIRE_COMMANDS.I2C_STOP,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.I2C_STOP, 1)
+
 	# 0000010x- CS low (0) / high (1)
 
 	def CS_Low(self):
-		return self.command(RAW_WIRE_COMMANDS.CS_LOW,1)
+		return self.command(RAW_WIRE_COMMANDS.CS_LOW, 1)
 
 	def CS_High(self):
-		return self.command(RAW_WIRE_COMMANDS.CS_HIGH,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.CS_HIGH, 1)
+
 	# 00000110 – Read byte
 	# Ummm: Reads a byte from the bus, returns the byte. Writes 0xff to bus in 3-wire mode.
 	# writes 0xff in 3-wire mode, in order to maintain the data line high?
-	
+
 	def read_byte(self):
-		return self.command(RAW_WIRE_COMMANDS.READ_BYTE,1)
-	
-	# 00000111 – Read bit	
+		return self.command(RAW_WIRE_COMMANDS.READ_BYTE, 1)
+
+	# 00000111 – Read bit
 	def read_bit(self):
-		return self.command(RAW_WIRE_COMMANDS.READ_BIT,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.READ_BIT, 1)
+
 	# 00001000 – Peek at input pin
-	
+
 	def peek(self):
-		return self.command(RAW_WIRE_COMMANDS.PEEK,1)
-	
+		return self.command(RAW_WIRE_COMMANDS.PEEK, 1)
+
 	# 00001001 – Clock tick
-	
+
 	def clk_tick(self):
-		return self.command(RAW_WIRE_COMMANDS.CLK_TICK,1)
-			
+		return self.command(RAW_WIRE_COMMANDS.CLK_TICK, 1)
+
 	# 0000101x – Clock low (0) / high (1)
 
 	def clk_low(self):
-		return self.command(RAW_WIRE_COMMANDS.CLK_LOW,1)
-	
+		return self.command(RAW_WIRE_COMMANDS.CLK_LOW, 1)
+
 	def clk_high(self):
-		return self.command(RAW_WIRE_COMMANDS.CLK_HIGH,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.CLK_HIGH, 1)
+
 	# 0000110x – Data low (0) / high (1)
 
 	def data_low(self):
-		return self.command(RAW_WIRE_COMMANDS.DATA_LOW,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.DATA_LOW, 1)
+
 	def data_high(self):
-		return self.command(RAW_WIRE_COMMANDS.DATA_HIGH,1)
-		
+		return self.command(RAW_WIRE_COMMANDS.DATA_HIGH, 1)
+
 	# 0001xxxx – Bulk transfer, send 1-16 bytes (0=1byte!)
 	#def bulk_trans(self, byte_count=1, byte_string=None):
 	#
 	# implemented on BitBang
-	
+
 	# 0010xxxx – Bulk clock ticks, send 1-16 ticks
-	
-	def bulk_clk(self, clk_count=0): # 0 == 1 clock
-		return self.command( chr(0x20 | clk_count), 1 );
-	
+
+	def bulk_clk(self, clk_count=0): 	# 0 == 1 clock
+		return self.command(chr(0x20 | clk_count), 1)
+
 	# 0100wxyz – Configure peripherals, w=power, x=pullups, y=AUX, z=CS
-	
+
 	# def cfg_pins(self, pins=0): at BitBang upper class
-	
+
 	# 0110000x – Set speed, low (0=~5kHz) / high (1=~50kHz) changed in v4.2
 	# 011000xx – Set speed, 3=~400kHz, 2=~100kHz, 1=~50kHz, 0=~5kHz
-	
+
 	#def set_speed(self, bus_speed=0): at BitBang upper class
-	
+
 	# 1000wxyz – Configure mode, w=output type, x=2/3wire, y=msb/lsb, z=n/a
-	
+
 	def cfg_raw_wire(self, raw_wire_cfg):
-		return self.command( chr(0x80 | raw_wire_cfg ), 1)
+		return self.command(chr(0x80 | raw_wire_cfg), 1)
 
 
